4ea1c26b5f808c8ccd9f98366e5f479248507043
Sam Evans <samuel.r.evans@boeing.com>
2019-02-23 20:55:27 -0500

    2.23.2019

---
+++ python/258 Add Digits.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        while len(str(num)) > 1:
+            num = sum(list(map(int,str(num))))
+        return num
