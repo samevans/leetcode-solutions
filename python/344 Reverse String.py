bdaf51b06110f877e8fc0bb7683ab1a6b5f875e3
Sam Evans <6379040+samevans@users.noreply.github.com>
2019-03-22 22:18:05 -0400

    Update 344 Reverse String.py

--- python/344 Reverse String.py
+++ python/344 Reverse String.py
@@ -2,13 +2,8 @@ class Solution(object):
     def reverseString(self, s):
         """
         :type s: List[str]
-        :rtype: None Do not return anything, modify s in-place instead.
+        :rtype: void Do not return anything, modify s in-place instead.
         """
-        left = 0
-        right = len(s)-1
-        
-        while left < right:
-            s[left],s[right] = s[right],s[left]
-            
-            left+=1
-            right-=1
+        n = len(s)
+        for i in range(n/2):
+            s[i], s[n-1-i] = s[n-1-i], s[i]
