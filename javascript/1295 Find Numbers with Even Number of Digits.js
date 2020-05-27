477e55dbcfe242e3a586fab9eaf5cdb70594db12
Sam Evans <smlevans@gmail.com>
2020-05-26 22:02:45 -1000

    javascript init

---
+++ javascript/1295 Find Numbers with Even Number of Digits.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNumbers = function(nums) {
+    let result = 0;
+    for(var i=0;i<nums.length;i++){
+        var length = Math.log(nums[i]) * Math.LOG10E + 1 | 0;
+        if (length%2===0) {
+            result++;
+        }
+    }
+    return result;
+};
