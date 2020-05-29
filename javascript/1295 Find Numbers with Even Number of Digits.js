d37331911b51cc0b723bbb3a23bfe0d9c72c63ae
Samuel Evans <samuelevans@MacBook-Pro.lan1>
2020-05-28 23:57:41 -1000

    May 28 2020

--- javascript/1295 Find Numbers with Even Number of Digits.js
+++ javascript/1295 Find Numbers with Even Number of Digits.js
@@ -6,7 +6,7 @@ var findNumbers = function(nums) {
     let result = 0;
     for(var i=0;i<nums.length;i++){
         var length = Math.log(nums[i]) * Math.LOG10E + 1 | 0;
-        if (length%2===0) {
+        if (length % 2 === 0) {
             result++;
         }
     }
