a696d4b160d8314c31c414a38020322092f41a82
sevans <sevans@revacomm.com>
2020-07-13 21:35:49 -1000

    July 13 2020

--- javascript/0680 Valid Palindrome II.js
+++ javascript/0680 Valid Palindrome II.js
@@ -6,24 +6,21 @@ var validPalindrome = function(s) {
 
     let left = 0, right = s.length-1;
 
-    let deleted = false;
-    while(left<right) {
-        if(s[left]===s[right]){
-            left+=1;
-            right-=1;
+    while(left<right){
+        if(s[left]!==s[right]){
+            return isValidPalindrome(s,left+1,right) || isValidPalindrome(s,left,right-1);
         }
-        else if(s[left+1]===s[right] && s[left+2]===s[right-1] && !deleted){
-            left+=1;
-            deleted=true;
-        }
-        else if(s[left]===s[right-1] && !deleted){
-            right-=1;
-            deleted=true;
-        }
-        else{
-            return false;
-        }
+        left++;
+        right--;
     }
     return true;
-
 };
+
+var isValidPalindrome = function(s,left,right){
+    while(left<right){
+        if(s[left]!==s[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
+}
