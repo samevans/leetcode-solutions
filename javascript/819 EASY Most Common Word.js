60ba673e85a2b7caceb1ecc87f036a3ae30eb875
sevans <sevans@revacomm.com>
2020-07-25 08:43:51 -1000

    July 25 2020

---
+++ javascript/819 EASY Most Common Word.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} paragraph
+ * @param {string[]} banned
+ * @return {string}
+ */
+var mostCommonWord = function(paragraph, banned) {
+    const bannedDict = {}
+    for(let bannedWord of banned){
+        bannedDict[bannedWord] = 1;
+    }
+
+    const count = {}
+    let words = paragraph.toLowerCase().replace(/[^a-z0-9]/gmi, " ").replace(/\s+/g, " ").split(" ");
+    for(let word of words){
+        if(!bannedDict[word]){
+            if(!count[word]){
+                count[word] = 0;
+            }
+            count[word]++;
+        }
+    }
+
+    let output = undefined;
+    for(let key of Object.keys(count)){
+        if(!output || count[key] > count[output]){
+            output = key;
+        }
+    }
+    return output;
+};
