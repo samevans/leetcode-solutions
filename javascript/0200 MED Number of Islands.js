00c2b2168eefb46d2fc71106c1ac186fc19ffd02
sevans <sevans@revacomm.com>
2020-08-25 13:19:08 -1000

    200

--- javascript/0200 MED Number of Islands.js
+++ javascript/0200 MED Number of Islands.js
@@ -3,55 +3,57 @@
  * @return {number}
  */
 var numIslands = function(grid) {
+    let num = 0;
+    const visited = new Set();
 
-    const visited = {};
-    let answer = 0;
-    for(var row=0;row<grid.length;row++){
-
-        for(var col=0;col<grid[0].length;col++){
-            if(grid[row][col]==='1' && !visited[row+'-'+col]){
-                dfs(visited, grid, [row,col]);
-                answer++;
+    for(let row=0;row<grid.length;row++){
+        for(let col=0;col<grid[0].length;col++){
+            if(grid[row][col]==="1" && !visited.has(row+'-'+col)){
+                dfs(grid,visited,row,col);
+                num++;
             }
         }
-
     }
-    return answer
+
+    return num;
 };
 
-var dfs = function(visited, grid, curr){
+var dfs = function(grid,visited,row,col){
+    const stack = [[row,col]];
 
-    let row = curr[0], col = curr[1];
-    visited[row+'-'+col] = 1;
+    while(stack.length){
+        let [r,c] = stack.pop();
 
-    let cells = [[row-1,col],[row,col-1],[row+1,col],[row,col+1]];
-    for(let cell of cells){
-        if(isValid(grid,visited,cell)){
-            dfs(visited, grid, cell);
+        if(visited.has(r+'-'+c)){
+            continue;
+        }
+        visited.add(r+'-'+c);
+
+        for(const [newRow, newCol] of [[r+1,c],[r-1,c],[r,c+1],[r,c-1]]){
+            if(isValid(grid, newRow, newCol)){
+                stack.push([newRow,newCol]);
+            }
         }
     }
 }
 
-var isValid = function(grid, visited, cell){
-    let row=cell[0], col=cell[1];
+var isValid = function(grid, row, col){
+
     if(row<0){
         return false;
     }
     else if(col<0){
         return false;
     }
-    else if(row>grid.length-1){
+    else if(row>=grid.length){
         return false;
     }
-    else if(col>grid[0].length-1){
+    else if(col>=grid[0].length){
         return false;
     }
-    else if(grid[row][col]==='0'){
-        return false;
-    }
-    else if(visited[row+'-'+col]){
+    else if(grid[row][col]!=="1"){
         return false;
     }
 
-    return true;
+    return true;    
 }
