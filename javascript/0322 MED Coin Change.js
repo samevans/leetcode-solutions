b6fb082e0a788aa6561701fbc3d574033dc5dd99
Sam Evans <samevans@protonmail.com>
2021-05-24 19:18:12 -1000

    322

--- javascript/0322 MED Coin Change.js
+++ javascript/0322 MED Coin Change.js
@@ -5,15 +5,16 @@
  */
 var coinChange = function(coins, amount) {
     let dp = Array(amount+1).fill(Infinity)
-    dp[0] = 0;
-
-    for(let i=0;i<=amount;i++){
-        for(const coin of coins){
-            if(coin<=i){
-                dp[i] = Math.min(dp[i], dp[i-coin]+1);
+    dp[0] = 0
+    
+    for(let i=0;i <= amount; i++){
+        for(coin of coins){
+            if(coin <= i){
+                dp[i] = Math.min(dp[i-coin]+1, dp[i])
             }
         }
     }
-
-    return dp[amount] === Infinity ? -1 : dp[amount];
+    
+    return dp[amount] === Infinity ? -1 : dp[amount]
+    
 };
