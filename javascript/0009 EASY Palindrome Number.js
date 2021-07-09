012874fde019bcc4c6b4a79e89aef71363e328ec
Sam Evans <samevans@protonmail.com>
2021-07-08 17:09:53 -1000

    9

---
+++ javascript/0009 EASY Palindrome Number.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    x = x.toString()
+    
+    let left = 0;
+    let right = x.length - 1
+    
+    while(left<right){
+        if(x[left]!==x[right]){
+            return false
+        }
+        left++
+        right--
+    }
+    
+    return true
+};
