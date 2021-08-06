435a5f1f96000d2ba51ac8d2cf50cf1aece96ae6
Sam Evans <samevans@protonmail.com>
2021-08-05 20:08:49 -1000

    953

--- javascript/0953 Verifying an Alien Dictionary.js
+++ javascript/0953 Verifying an Alien Dictionary.js
@@ -4,26 +4,35 @@
  * @return {boolean}
  */
 var isAlienSorted = function(words, order) {
-    let alienDict = {}
-    for(var i=0;i<order.length;i++){
-        alienDict[order[i]]=i;
+    let dict = {}
+    for(let i=0; i<order.length; i++){
+        dict[order[i]] = i
     }
-
-    for(var i =1; i<words.length; i++){
-        const curr = words[i], prev = words[i-1];
-        let ptr = 0;
-        while(!(alienDict[curr[ptr]]>alienDict[prev[ptr]])){
-            if(!prev[ptr]){
-                break;
-            }
-            else if(!curr[ptr]){
-                return false;
-            }
-            else if(alienDict[curr[ptr]]<alienDict[prev[ptr]]){
-                return false;
-            }
-            ptr++;
+    
+    for(let j=1;j<words.length;j++){
+        if(!isSorted(words[j-1],words[j],dict)){
+            return false
         }
     }
-    return true;
+    return true
 };
+
+var isSorted = function(word1, word2, dict){
+    let i=0;
+    while(i<word1.length || i<word2.length){
+        if(i>=word2.length){
+            return false
+        }
+        else if(i>=word1.length){
+            return true
+        }
+        else if(dict[word1[i]]<dict[word2[i]]){
+            return true
+        }
+        else if(dict[word2[i]]<dict[word1[i]]){
+            return false
+        }
+        i++
+    }
+    return true
+}
